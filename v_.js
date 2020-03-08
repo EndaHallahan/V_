0474030ff31a3be6b4ee5dd93a50e3253def4fb7
Enda Hallahan <41527196+EndaHallahan@users.noreply.github.com>
2020-03-08 17:45:54 -0500

    Update v_.js

--- v_.js
+++ v_.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -91,17 +91,36 @@
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return v_ify; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return V_; });
+/* harmony import */ var _defaultMessages_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+var _defaultMessages_json__WEBPACK_IMPORTED_MODULE_0___namespace = /*#__PURE__*/__webpack_require__.t(1, 1);
+
+
 class V_ {
-	constructor(form) {
-		if (form) {
+	constructor(formOrOpts, opts) {
+		this.options = undefined;
+		this.messages = undefined;
+		this.defaultMessages = _defaultMessages_json__WEBPACK_IMPORTED_MODULE_0__;
+		if (formOrOpts && formOrOpts.nodeName && formOrOpts.nodeName === "FORM") {
+			let form = formOrOpts;
 			this.setValidations(form);
+			if (opts) {
+				this.options = opts;
+				if (opts.messages) {
+					this.messages = opts.messages
+				}
+			}
+		} else if (formOrOpts && !formOrOpts.nodeName) {
+			this.options = formOrOpts;
+			if (formOrOpts.messages) {
+				this.messages = formOrOpts.messages
+			}
 		}
 	}
 
 	// Calls validatorBuilder on form inputs.
 	setValidations(form) {
 		form.querySelectorAll("input, textarea, datalist, label").forEach(input => {
-			this.validatorBuilder(input)
+			this.validatorBuilder(input, this.defaultMessages);
 		});
 	}
 
@@ -110,10 +129,18 @@ class V_ {
 	validatorBuilder(input) {
 		let validations = input.dataset;
 		let validationSet = [];
+		let messageSet = [];
 		for (let filter in validations) {
 			if (filter.startsWith("v_")) {
 				if (this[filter]) {
 					validationSet.push(this[filter]);
+					if (this.messages && this.messages[filter]) {
+						messageSet.push(this.messages[filter]);
+					} else if (this.defaultMessages[filter]) {
+						messageSet.push(this.defaultMessages[filter]);
+					} else {
+						throw new Error(`Missing a fail message for '${filter}'`);
+					}
 				} else {
 					console.warn(`Validation warning: V_ did not recognize attribute '${filter}', skipping...`)
 				}
@@ -121,17 +148,17 @@ class V_ {
 		}
 		if (validationSet.length) {
 			input.addEventListener("input", (e) => {
-				this.validator(e.target, validationSet);
+				this.validator(e.target, validationSet, messageSet);
 			});
 		}
 	}
 
 	// Runs through an element's array of validations constructed by validatorBuilder.
-	validator(input, validationSet) {
+	validator(input, validationSet, messageSet) {
 		let content = input.value.trim();
 		let reset = true;
 		for (let i = 0; i < validationSet.length; i++) {
-			let check = validationSet[i](input, content);
+			let check = validationSet[i](input, content, messageSet[i]);
 			if (check === false) {
 				reset = false;
 				return;
@@ -143,7 +170,6 @@ class V_ {
 		if (reset) {
 			input.setCustomValidity("");
 		}
-		
 	}
 
 	// Adds a custom validation function to the V_ object.
@@ -152,79 +178,77 @@ class V_ {
 	// 	validationFunction(inputValue [, dataAttributeValue]) - Function to be evaluated. Must return a boolean.
 	// 	failMessage - The message to be displayed to the user if the validation fails.
 	addCustomValidation(validObj) {
-		try {
-			let funcName = validObj.name;
-			let validFunc = validObj.validationFunction;
-			let errorMessage = validObj.failMessage ? validObj.failMessage : "Input does not meet requirements.";
-			let newValidation = function (input, content) {
-				let valiVal = input.dataset[funcName];
-				if (!validFunc(content, valiVal)) {
-					input.setCustomValidity(errorMessage);
-					return false;
-				} else {
-					return true;
-				}
+		let funcName = validObj.name;
+		let validFunc = validObj.validationFunction;
+		let errorMessage = validObj.failMessage ? validObj.failMessage : this.defaultMessages.generic;
+		if (!this.messages) {this.messages = {};}
+		this.messages[funcName] = errorMessage;
+		let newValidation = function (input, content, message) {
+			let valiVal = input.dataset[funcName];
+			if (!validFunc(content, valiVal)) {
+				input.setCustomValidity(message);
+				return false;
+			} else {
+				return true;
 			}
-			this[funcName] = newValidation;
-			return newValidation;
-		} catch {
-			return null;
 		}
+		this[funcName] = newValidation;
+		return newValidation;
 	}
 
 	/*-- VALIDATION FUNCTIONS --*/
 
-	v_isNumber(input, content) {
+	v_isNumber(input, content, message) {
 		if (content === "") {return true;}
 		if (isNaN(parseFloat(content))) {
-			input.setCustomValidity("Input must be a number.");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isInteger(input, content) {
+	v_isInteger(input, content, message) {
 		if (content === "") {return true;}
 		let val = parseFloat(content);
 		if (isNaN(val) || !Number.isSafeInteger(val)) {
-			input.setCustomValidity("Input must be a whole number.");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isAlphanumeric(input, content) {
+	v_isAlphanumeric(input, content, message) {
 		if (content === "") {return true;}
 		if (typeof content !== "string" || !/^[\da-zA-Z]*$/.test(content)) {
-			input.setCustomValidity("Input may only contain letters and numbers.");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isEmail(input, content) {
+	v_isEmail(input, content, message) {
 		if (content === "") {return true;}
 		if (typeof content !== "string" 
 			|| !/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(content)
 		) {
-			input.setCustomValidity("Email must be a vaild email address. Ex: user@domain.net");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isEmailWithDomain(input, content) {
+	v_isEmailWithDomain(input, content, message) {
 		if (content === "") {return true;}
 		let rawDomain = input.dataset.v_isEmailWithDomain;
 		if (rawDomain) {
 			let regStr = `^([a-zA-Z0-9_\\-\\.]+)@${rawDomain.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}$`;
 			let regex = new RegExp(regStr);
 			if (typeof content !== "string" || !regex.test(content)) {
-				input.setCustomValidity(`Input must be a valid email address from '${rawDomain}'.`);
+				input.setCustomValidity(message.replace("[arg]", rawDomain));
 				return false;
 			} else {
 				return true;
@@ -235,38 +259,38 @@ class V_ {
 		}
 	}
 
-	v_isUrl(input, content) {
+	v_isUrl(input, content, message) {
 		if (content === "") {return true;}
 		if (typeof content !== "string" 
 			|| !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(content)
 		) {
-			input.setCustomValidity("Input must be a vaild web address. Ex: https://example.com");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isSecureUrl(input, content) {
+	v_isSecureUrl(input, content, message) {
 		if (content === "") {return true;}
 		if (typeof content !== "string" 
 			|| !/https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(content)
 		) {
-			input.setCustomValidity("Input must be an SSL-secured web address beginning with 'https://'.");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_isUrlWithDomain(input, content) {
+	v_isUrlWithDomain(input, content, message) {
 		if (content === "") {return true;}
 		let rawDomain = input.dataset.v_isUrlWithDomain;
 		if (rawDomain) {
 			let regStr = `https?:\\/\\/(www\.)?${rawDomain.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)$`;
 			let regex = new RegExp(regStr);
 			if (typeof content !== "string" || !regex.test(content)) {
-				input.setCustomValidity(`Input must be a valid web address from '${rawDomain}'.`);
+				input.setCustomValidity(message.replace("[arg]", rawDomain));
 				return false;
 			} else {
 				return true;
@@ -277,25 +301,25 @@ class V_ {
 		}
 	}
 
-	v_isImageUrl(input, content) {
+	v_isImageUrl(input, content, message) {
 		if (content === "") {return true;}
 		if (typeof content !== "string" 
 			|| !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)\.(apng|gif|jpg|jpeg|jfif|pjpeg|pjp|png|svg|webp)$/.test(content)
 		) {
-			input.setCustomValidity("Input must be a URL to an image file. Accepted file types are: APNG, GIF, JPEG, PNG, SVG, WebP");
+			input.setCustomValidity(message);
 			return false;
 		} else {
 			return true;
 		}
 	}
 
-	v_valueLessThan(input, content) {
+	v_valueLessThan(input, content, message) {
 		if (content === "") {return true;}
 		let max = parseInt(input.dataset.v_valueLessThan);
 		if (!isNaN(max)) {
 			let val = parseFloat(content);
 			if (isNaN(val) || val >= max) {
-				input.setCustomValidity(`Value must be a number below ${max}`);
+				input.setCustomValidity(message.replace("[arg]", max));
 				return false;
 			} else {
 				return true;
@@ -306,13 +330,13 @@ class V_ {
 		}
 	}
 
-	v_valueGreaterThan(input, content) {
+	v_valueGreaterThan(input, content, message) {
 		if (content === "") {return true;}
 		let min = parseInt(input.dataset.v_valueGreaterThan);
 		if (!isNaN(min)) {
 			let val = parseFloat(content);
 			if (isNaN(val) || val <= min) {
-				input.setCustomValidity(`Value must be a number above ${min}`);
+				input.setCustomValidity(message.replace("[arg]", min));
 				return false;
 			} else {
 				return true;
@@ -323,7 +347,7 @@ class V_ {
 		}
 	}
 
-	v_valueBetween(input, content) {
+	v_valueBetween(input, content, message) {
 		if (content === "") {return true;}
 		let args = input.dataset.v_valueBetween.split(",");
 		let min = parseFloat(args[0]);
@@ -331,7 +355,7 @@ class V_ {
 		if (!isNaN(min) && !isNaN(max)) {
 			let val = parseFloat(content);
 			if (isNaN(val) || val <= min || val >= max) {
-				input.setCustomValidity(`Value must be a number between ${min} and ${max}`);
+				input.setCustomValidity(message.replace("[arg]", min).replace("[arg]", max));
 				return false;
 			} else {
 				return true;
@@ -342,12 +366,12 @@ class V_ {
 		}
 	}
 
-	v_lengthLessThan(input, content) {
+	v_lengthLessThan(input, content, message) {
 		if (content === "") {return true;}
 		let max = parseInt(input.dataset.v_lengthLessThan);
 		if (!isNaN(max)) {
 			if (typeof content !== "string" || content.length >= max) {
-				input.setCustomValidity(`Input must be shorter than ${max} characters.`);
+				input.setCustomValidity(message.replace("[arg]", max));
 				return false;
 			} else {
 				return true;
@@ -358,12 +382,12 @@ class V_ {
 		}
 	}
 
-	v_lengthGreaterThan(input, content) {
+	v_lengthGreaterThan(input, content, message) {
 		if (content === "") {return true;}
 		let min = parseInt(input.dataset.v_lengthGreaterThan);
 		if (!isNaN(min)) {
 			if (typeof content !== "string" || content.length <= min) {
-				input.setCustomValidity(`Input must be longer than ${min} characters.`);
+				input.setCustomValidity(message.replace("[arg]", min));
 				return false;
 			} else {
 				return true;
@@ -374,14 +398,14 @@ class V_ {
 		}
 	}
 
-	v_lengthBetween(input, content) {
+	v_lengthBetween(input, content, message) {
 		if (content === "") {return true;}
 		let args = input.dataset.v_lengthBetween.split(",");
 		let min = parseInt(args[0]);
 		let max = parseInt(args[1]);
 		if (!isNaN(min) && !isNaN(max)) {
 			if (typeof content !== "string" || content.length <= min || content.length >= max) {
-				input.setCustomValidity(`Input must be between ${min} and ${max} characters.`);
+				input.setCustomValidity(message.replace("[arg]", min).replace("[arg]", max));
 				return false;
 			} else {
 				return true;
@@ -392,7 +416,7 @@ class V_ {
 		}
 	}
 
-	v_matchesRegex(input, content) {
+	v_matchesRegex(input, content, message) {
 		if (content === "") {return true;}
 		let rawRegex = input.dataset.v_matchesRegex;
 		if (rawRegex) {
@@ -401,7 +425,7 @@ class V_ {
 			}
 			let regex = new RegExp(rawRegex);
 			if (typeof content !== "string" || !regex.test(content)) {
-				input.setCustomValidity("Input does not match requirements.");
+				input.setCustomValidity(message);
 				return false;
 			} else {
 				return true;
@@ -412,7 +436,7 @@ class V_ {
 		}
 	}
 
-	v_matchesField(input, content) {
+	v_matchesField(input, content, message) {
 		if (content === "") {return true;}
 		let fieldName = input.dataset.v_matchesField;
 		if (fieldName) {
@@ -422,7 +446,7 @@ class V_ {
 				if (comparisonField) {
 					let comparison = comparisonField.value.trim();
 					if (comparison !== content) {
-						input.setCustomValidity(`Input does not match.`);
+						input.setCustomValidity(message);
 						return false;
 					} else {
 						return true;
@@ -441,7 +465,7 @@ class V_ {
 		}
 	}
 
-	v_passwordStrengthRequirement(input, content) {
+	v_passwordStrengthRequirement(input, content, message) {
 		if (content === "") {return true;}
 		let req = parseInt(input.dataset.v_passwordStrengthRequirement);
 		if (!isNaN(req) && req >= 0 && req <= 4) {
@@ -456,20 +480,20 @@ class V_ {
 				];
 				sets.forEach(set => {if (set.regex.test(content)) {sumset += set.size}});
 				if (sumset === 0) {
-					input.setCustomValidity("Password is too weak. Consider using a longer password or passphrase, or including a greater variety of characters.");
+					input.setCustomValidity(message);
 					return false;
 				}
 				let entropy = ((Math.log(sumset) / Math.log(2)) * content.length);
 				let strength = Math.floor(entropy/30);
 				input.dataset.v_passwordStrength = strength <= 4 ? strength.toString() : "4";
 				if (strength <= req) {
-					input.setCustomValidity("Password is too weak. Consider using a longer password or passphrase, or including a greater variety of characters.");
+					input.setCustomValidity(message);
 					return false;
 				} else {
 					return true;
 				}
 			} else {
-				input.setCustomValidity("Password is too weak. Consider using a longer password or passphrase, or including a greater variety of characters.");
+				input.setCustomValidity(message);
 				return false;
 			}	
 		} else {
@@ -478,12 +502,12 @@ class V_ {
 		}
 	}
 
-	v_passwordZxcvbnRequirement(input, content) {
+	v_passwordZxcvbnRequirement(input, content, message) {
 		if (content === "") {return true;}
 		// Determine whether zxcvbn is available, either globally on the window or as a CommonJS module
 		let vbn;
-		try {vbn = zxcvbn;} catch {}
-		try {vbn = __webpack_require__(1);}  catch {}
+		try {vbn = zxcvbn;} catch {try {vbn = __webpack_require__(2);}  catch {}}
+		
 		if (!vbn) {
 			input.setCustomValidity("An error has occurred. Please check the console for details.");
 			throw new Error("Validation error: Use of v_password-zxcvbn-requirement requires zxcvbn.js: https://github.com/dropbox/zxcvbn\n" + input);
@@ -495,7 +519,7 @@ class V_ {
 				let strength = zxc.score;
 				input.dataset.v_passwordStrength = strength;
 				if (strength <= req) {
-					input.setCustomValidity("Password is too weak."
+					input.setCustomValidity("message"
 						+ (zxc.feedback && zxc.feedback.warning ? " \nWarning: " + zxc.feedback.warning : "")
 						+ (zxc.feedback && zxc.feedback.suggestions ? " \nSuggestions:\n -" + zxc.feedback.suggestions.join("\n -") : "")
 					);
@@ -504,7 +528,7 @@ class V_ {
 					return true;
 				}
 			} else {
-				input.setCustomValidity("Password is too weak. Consider using a longer password or passphrase, or including a greater variety of characters.");
+				input.setCustomValidity(message);
 				return false;
 			}
 		} else {
@@ -515,7 +539,7 @@ class V_ {
 
 	/*-- UI FUNCTIONS --*/
 
-	v_charCounter(input) {
+	v_charCounter(input, content, message) {
 		let label = input.parentNode;
 		let max;
 		if (input.maxLength > 0) {
@@ -526,9 +550,8 @@ class V_ {
 			input.setCustomValidity("An error has occurred. Please check the console for details.");
 			throw new Error("Validation error: Input must have either a 'maxlength' or 'v_length-less-than' attribute\n" + input);
 		}
-		let content = input.value.trim();
 		if (!isNaN(max)) {
-			label.setAttribute('data-v_char-counter', `${content.length}/${max}`);
+			label.setAttribute('data-v_char-counter', message.replace("[arg]", content.length).replace("[arg]", max));
 			return null;
 		} else {
 			input.setCustomValidity("An error has occurred. Please check the console for details.");
@@ -564,13 +587,19 @@ function v_ify(form) {
 
 /***/ }),
 /* 1 */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"generic\":\"Input does not meet requirements.\",\"v_isNumber\":\"Input must be a number.\",\"v_isInteger\":\"Input must be a whole number.\",\"v_isAlphanumeric\":\"Input may only contain letters and numbers.\",\"v_isEmail\":\"Email must be a vaild email address. Ex: user@domain.net\",\"v_isEmailWithDomain\":\"Input must be a valid email address from '[arg]'\",\"v_isUrl\":\"Input must be a vaild web address. Ex: https://example.com\",\"v_isSecureUrl\":\"Input must be an SSL-secured web address beginning with 'https://'.\",\"v_isUrlWithDomain\":\"Input must be a vaild web address from '[arg]'\",\"v_isImageUrl\":\"Input must be a URL to an image file. Accepted file types are: APNG, GIF, JPEG, PNG, SVG, WebP\",\"v_valueLessThan\":\"Value must be a number below [arg]\",\"v_valueGreaterThan\":\"Value must be a number above [arg]\",\"v_valueBetween\":\"Value must be a number between [arg] and [arg]\",\"v_lengthLessThan\":\"Input may not be longer than [arg] characters.\",\"v_lengthGreaterThan\":\"Input may not be shorter than [arg] characters.\",\"v_lengthBetween\":\"Input must be between [arg] and [arg] characters in length.\",\"v_matchesRegex\":\"Input does not meet requirements.\",\"v_matchesField\":\"Input does not match.\",\"v_passwordStrengthRequirement\":\"Password is too weak. Consider using a longer password or passphrase, or including a greater variety of characters.\",\"v_passwordZxcvbnRequirement\":\"Password is too weak.\",\"v_charCounter\":\"[arg] / [arg]\",\"v_errorOutput\":\" \",\"v_passwordOutput\":\" \"}");
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports) {
 
 if(typeof zxcvbn === 'undefined') {var e = new Error("Cannot find module 'zxcvbn'"); e.code = 'MODULE_NOT_FOUND'; throw e;}
 module.exports = zxcvbn;
 
 /***/ }),
-/* 2 */
+/* 3 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
